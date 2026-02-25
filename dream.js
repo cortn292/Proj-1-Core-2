0f500ac6fcf1567df8eec591cee5dcfdea15a8d8
Nat <cortn292@newschool.edu>
2026-02-24 23:13:40 -0500

    Struggling with JS

--- dream.js
+++ dream.js
@@ -4,9 +4,8 @@ let currentRotation = 0;
 let startAngle = 0;
 let currentScale = 1;
 
-function getcenter(el) {
-    const r = el.getBoundingClientRect();
-    return { x: r.left + r.width / 2, y: r.top + r.height / 2};
-}
+function getAngle(el) {
+    const 
+})
 
 
