41cde1778388c8474291a405dbe65fbeeed04f3a
Nat <cortn292@newschool.edu>
2026-02-24 23:15:01 -0500

    adding the interactivity with the mouse

--- dream.js
+++ dream.js
@@ -5,7 +5,21 @@ let startAngle = 0;
 let currentScale = 1;
 
 function getAngle(el) {
-    const 
-})
+    const rect = circle.getBoundingClientRect();
+    const dx = x - (rect.left + rect.width / 2);
+    const dy = y - (rect.top + rect.height / 2);
+    return Math.atan2(dy, dx) * (180 / Math.PI);
+}
+
+function update() {
+    circle.style.transform = `rotate(${rotation}deg) scale(${scale})`;
+}
+
+circle.addEventListener('mousedown', function(e) {
+    dragging = true;
+    startAngle = angleFromCenter(e.clientX, e.clientY) - rotation;
+});
+
+  
 
 
