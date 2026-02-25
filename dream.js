5a1466ebb012dabdd0009ac0ac8d04f406218cd1
Nat <cortn292@newschool.edu>
2026-02-24 23:06:14 -0500

    redoing Javascript circular ruins

--- dream.js
+++ dream.js
@@ -1,2 +1,12 @@
+const circleContainer = document.getElementById('circleContainer');
+let isDragging = false;
+let currentRotation = 0;
+let startAngle = 0;
+let currentScale = 1;
+
+function getcenter(el) {
+    const r = el.getBoundingClientRect();
+    return { x: r.left + r.width / 2, y: r.top + r.height / 2};
+}
 
 
